b191c54b22c863f593854f1fe85c0a6d9f75a6fc
revaniputeri <revaninandaputeri@gmail.com>
2023-09-27 12:51:10 +0700

    Memodifikasi ifelse menggunakan Ternary Operator

--- jobsheet5/java/PemilihanPercobaan124.java
+++ jobsheet5/java/PemilihanPercobaan124.java
@@ -8,10 +8,13 @@ public class PemilihanPercobaan124 {
         System.out.print("Masukkan angka : ");
         int angka = input24.nextInt();
 
-        if (angka %2 == 0) {
-            System.out.println("Angka "+angka+" bilangan genap");
-        } else {
-           System.out.println("Angka "+angka+" bilangan ganjil"); 
-        }
+        // if (angka %2 == 0) {
+        //     System.out.println("Angka "+angka+" bilangan genap");
+        // } else {
+        //    System.out.println("Angka "+angka+" bilangan ganjil"); 
+        // }
+
+        String result = (angka %2 == 0) ? "Angka "+angka+" bilangan genap" : "Angka "+angka+" bilangan ganjil";
+        System.out.println(result);
     }
 }
