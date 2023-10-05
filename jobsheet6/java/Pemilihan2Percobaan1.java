36ea596aadfa964e141ee0167269c98675e46005
revaniputeri <revaninandaputeri@gmail.com>
2023-10-05 13:48:06 +0700

    :fire: Q no4

--- jobsheet6/java/Pemilihan2Percobaan1.java
+++ jobsheet6/java/Pemilihan2Percobaan1.java
@@ -9,12 +9,17 @@ public class Pemilihan2Percobaan1 {
         System.out.print("Masukkan tahun: ");
         tahun = input24.nextInt();
         if ((tahun %4 ) == 0) {
-            if ((tahun %100) != 0) {
-                System.out.println("Tahun Kabisat");   
+            if ((tahun %100) == 0) {
+                if ((tahun %400) == 0) {
+                    System.out.println("Tahun Kabisat");      
+                }else{
+                    System.out.println("Bukan Tahun Kabisat");
+                }   
             }else{
-                System.out.println("Bukan Tahun Kabisat");
+                System.out.println("Tahun Kabisat");
             }
         } else {
             System.out.println("Bukan Tahun Kabisat");
-        }    }
+        }    
+    }
 }
