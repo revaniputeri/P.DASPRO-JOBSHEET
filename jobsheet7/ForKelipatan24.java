ddf63ec0367af5e75b713b1bee2097d43a82988a
revaniputeri <revaninandaputeri@gmail.com>
2023-10-12 12:52:53 +0700

    avg

--- jobsheet7/ForKelipatan24.java
+++ jobsheet7/ForKelipatan24.java
@@ -4,17 +4,19 @@ public class ForKelipatan24 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int kelipatan, jumlah = 0, counter = 0, total;
+        int kelipatan, jumlah = 0, counter = 0, average = 0;
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
         for (int i = 1; i <= 50; i++) {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter++;
+                average = jumlah/counter;
             }
         }
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata - rata bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, average);
 
     }
 }
