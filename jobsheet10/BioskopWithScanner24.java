85f9866cae4cad3ed076e72533fbc81aae720e93
revaniputeri <revaninandaputeri@gmail.com>
2023-11-02 15:33:57 +0700

    percobaan 3

--- jobsheet10/BioskopWithScanner24.java
+++ jobsheet10/BioskopWithScanner24.java
@@ -6,6 +6,7 @@ public class BioskopWithScanner24 {
         int baris, kolom;
         String nama, next, userChoice;
         String penonton[][] = new String[4][2];
+        boolean validInput = false;
         boolean running = false;
         while (!running) {
             System.out.println("");
@@ -18,7 +19,8 @@ public class BioskopWithScanner24 {
             userChoice = sc.nextLine();
 
             if (userChoice.equalsIgnoreCase("1")) {
-                while (true) {
+                next = "y";
+                while (next.equalsIgnoreCase("y")) {
                     System.out.print("Masukkan nama: ");
                     nama = sc.nextLine();
                     System.out.print("Masukkan baris: ");
@@ -27,19 +29,25 @@ public class BioskopWithScanner24 {
                     kolom = sc.nextInt();
                     sc.nextLine();
 
+                    if (penonton[baris - 1][kolom - 1] != null) {
+                        System.out.println("Kursi ini sudah ditempati oleh penonton lain.");
+                        continue;
+                    }
+
                     penonton[baris - 1][kolom - 1] = nama;
 
                     System.out.print("Input penonton lainnya? (y/n): ");
                     next = sc.nextLine();
-
-                    if (next.equalsIgnoreCase("n")) {
-                        break;
-                    }
                 }
             } else if (userChoice.equalsIgnoreCase("2")) {
                 System.out.println("Daftar penonton bioskop");
                 for (int i = 0; i < penonton.length; i++) {
+                    if (penonton[i][0] == null || penonton [i][1] == null) {
+                        penonton[i][0] = "***";
+                        penonton[i][1] = "***";
+                    }
                     System.out.println("Penonton pada baris ke-" + (i + 1) + ": " + String.join(", ", penonton[i]));
+                    
                 }
                 continue;
             } else {
