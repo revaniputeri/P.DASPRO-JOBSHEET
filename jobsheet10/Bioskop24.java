5f191aa3347201b601eb93d567eb3fbdbff15322
revaniputeri <revaninandaputeri@gmail.com>
2023-11-02 12:42:25 +0700

    modifikasi array loop

--- jobsheet10/Bioskop24.java
+++ jobsheet10/Bioskop24.java
@@ -5,6 +5,7 @@ public class Bioskop24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String penonton[][] = new String[4][2];
+
         penonton[0][0] = "Amin";
         penonton[0][1] = "Bena";
         penonton[1][0] = "Candra";
@@ -12,11 +13,18 @@ public class Bioskop24 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
 
         System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
+
+        System.out.println(penonton.length);
+        
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + penonton[i].length);
+        }
 
     }
 }
