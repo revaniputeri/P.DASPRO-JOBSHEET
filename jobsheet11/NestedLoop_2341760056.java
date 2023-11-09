56acfd45479b72e6b8668a0abc09f50aa1067595
revaniputeri <revaninandaputeri@gmail.com>
2023-11-09 19:04:58 +0700

    modif 5.1

--- jobsheet11/NestedLoop_2341760056.java
+++ jobsheet11/NestedLoop_2341760056.java
@@ -17,8 +17,11 @@ public class NestedLoop_2341760056 {
 
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + (i + 1) + ": ");
-            for (int j = 0; j < temps[0].length; j++) {
-                System.out.print(temps[i][j] + " ");
+            for (double[] ds : temps) {
+                // for (int j = 0; j < temps[0].length; j++) {
+                //     System.out.print(temps[i][j] + " ");
+                // }
+                System.out.print(ds[i] + " ");
             }
             System.out.println();
         }
