a06f8bb2af1a7dced59b710dd5b089ff6e54f3e2
revaniputeri <revaninandaputeri@gmail.com>
2023-11-09 13:09:53 +0700

    modifikasi code 1

---
+++ jobsheet11/Star.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Star {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in); //deklarasi scanner untuk inputan
+
+        //inputan banyak angka
+        System.out.print("Masukkan nilai N: "); 
+        int N = sc.nextInt();
+
+        //perulangan untuk menampilkan * sebanyak nilai N
+        for (int i = 0; i <= N; i++) {
+            System.out.print("*");
+        }
+    }
+}
