ea6e38b02926b2265c929646365a939dc08bcba2
revaniputeri <revaninandaputeri@gmail.com>
2023-11-09 19:13:57 +0700

    modif 5.2

--- jobsheet11/NestedLoop_2341760056.java
+++ jobsheet11/NestedLoop_2341760056.java
@@ -17,11 +17,9 @@ public class NestedLoop_2341760056 {
 
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + (i + 1) + ": ");
-            for (double[] ds : temps) {
-                // for (int j = 0; j < temps[0].length; j++) {
-                //     System.out.print(temps[i][j] + " ");
-                // }
-                System.out.print(ds[i] + " ");
+            // Menggunakan foreach untuk menampilkan array
+            for (double temp : temps[i]) {
+                System.out.print(temp + " ");
             }
             System.out.println();
         }
